40a2e4f197f9bd50bc5d8a367a8a6029706f1ee4
samuelaidoo22 <123897745+samuelaidoo22@users.noreply.github.com>
2025-08-18 17:16:19 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-let introduction = "Hello, my name is Samuel Aidoo. I'm learning JavaScript.";
 
+
+// JAVASCRIPT//
+
+// variables
+
+//ways to create variables: var, let, const
+
+let firstName= "Samuel"
 let age = 24;
+const GENDER = "MALE"
+let occupation = "Mobile developer"
 
-let gender = "male";
+//console.log() = display something to the console 
+console.log(firstname)
+console.log(age)
 
-let location = "Ghana";
+// DATA TYPES...
+// WE HAVE TWO DATA TYPE = PRIMITIVE AND REFERENCE
+
+// PRIMITIVE DATA TYPES
+// 1. STRING
+// 2. NUMBER
+// 3. BOOLEAN
+// 4. NULL
+// 5. UNDEFINED
+// 6. SYMBOL
+
+// REFERENCE DATA TYPES
+// 1. OBJECT
+// 2. ARRAY
+// 3. FUNCTION
