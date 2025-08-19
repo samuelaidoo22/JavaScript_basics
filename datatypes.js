c7665fc0b73cda33d0a3c7746d4c568c509f3391
samuelaidoo22 <123897745+samuelaidoo22@users.noreply.github.com>
2025-08-19 00:44:40 +0000

    Data types
    Javascript datatypes

---
+++ datatypes.js
@@ -0,0 +1,61 @@
+// DATA TYPES...
+// WE HAVE TWO DATA TYPE = PRIMITIVE AND REFERENCE
+
+// PRIMITIVE DATA TYPES
+// 1. STRING
+// 2. NUMBER
+// 3. BOOLEAN
+// 4. NULL
+// 5. UNDEFINED
+// 6. SYMBOL
+
+// string = Anything inside a single or double quotes
+//eg.
+let firstname , bestcolor, gender, bill;
+
+firstname = 'samuel'
+lastname = 'Aidoo'
+bestcolor = 'blue'
+gender = 'male'
+bill ="1000000"
+
+// numbers
+let age = 24
+let cgpa = 3.5
+let accountBalance = 10000
+
+// booleans = true or false
+let isMarried = false
+let hasChildren = true
+
+//////
+let name;
+
+//console.log('Name:', name) // undefined
+
+// type of operation
+//operations on strings
+// concatenation
+let fullname = firstname + " " + lastname;
+//console.log("Fullname:", fullname)
+
+//operations on numbers
+//add(+), subtract(-), multiply(*), divide(/), get remainder(%)
+
+//console.log (5+5)
+//console.log (5-5)
+//console.log (5*5)
+//console.log (5/5)
+//console.log (5%5)
+
+Math.log10(100) // 2
+console.log(Math.log10(100))
+console.log(Math.sqrt(100)) // 10
+
+
+
+
+// REFERENCE DATA TYPES
+// 1. OBJECT
+// 2. ARRAY
+// 3. FUNCTION
