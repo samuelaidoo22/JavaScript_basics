32acd370b1a9f241638b5b871205f55d306242e4
samuelaidoo22 <123897745+samuelaidoo22@users.noreply.github.com>
2025-08-19 00:44:58 +0000

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,31 @@
+
+
+// JAVASCRIPT//
+
+// variables
+
+//ways to create variables: var, let, const
+
+let firstName= "Samuel"
+let age = 24;
+const GENDER = "MALE"
+let occupation = "Mobile developer"
+
+//console.log() = display something to the console 
+//console.log(firstname)
+//console.log(age)
+
+
+
+
+// reassignment
+let Firstname = 'Mercy'
+let Lastname = 'Addition'
+let Age = 26;
+
+// mercy got married
+
+Lastname = 'Aidoo'
+Age = 28;
+console.log ('Mercy marriage information:', Firstname , Lastname)
+console.log ('Mercy age:', Age)
